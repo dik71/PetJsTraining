6978415309789932b86e8beaacfd7fbcdd99aba3
Dmitry Kisel <dik100695@gmail.com>
2021-04-09 13:17:44 +0300

    first

---
+++ js/appealUser.js
@@ -0,0 +1,44 @@
+"use strict";
+
+
+
+// let number = 5.6;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(4/0);
+
+
+// const persone = "Alex";
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+
+// };
+
+//console.log(obj.name);
+//console.log(obj ["name"]);
+
+
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp',{}, []];
+// console.log(arr[1])
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "18");
+// console.log(answer);
+
+const answers = [];
+
+// answers[0] = prompt('Как ваше имя?');
+// answers[1] = prompt('Как вашф фамилия?');
+// answers[2] = prompt('Сколько вам лет?');
+
+// document.write(answers);
+console.log(typeof(answers));
+
+
