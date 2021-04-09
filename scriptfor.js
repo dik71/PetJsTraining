1a6968cdb3f3d9bd1626201f94be5b1e16c9a458
Dmitry Kisel <dik100695@gmail.com>
2021-04-09 20:06:35 +0300

    Цыклы

--- scriptfor.js
+++ scriptfor.js
@@ -1,3 +1,28 @@
 "use strict";
 
-const num = 50;
+let num = 50;
+
+// while (num < 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
+
+// for (let i = 0; i < 8; i++) {
+//     console.log(num);
+//     num++;    
+// }
+
+for (let i = 0; i < 10; i++) {
+    if (i === 6) {
+        //break;
+        continue;
+    }    
+
+    console.log(i);
+}
