01ff98319117ec78e30bd70866904327a9b0a803
Dmitry Kisel <dik100695@gmail.com>
2021-04-09 16:54:51 +0300

    Условия if switch

---
+++ scriptIf.js
@@ -0,0 +1,39 @@
+"use strict";
+
+// if (4 == 9) {
+//     console.log('Ok'); //true
+// } else {
+//     console.log('Error'); //false
+// }
+
+// const num = 50;
+
+// if (num < 50) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Много');
+// } else {
+//     console.log('ok!');
+// }
+
+// // Тернарный оператор
+
+// (num === 50) ? console.log('Ok!') : console.log('Error');
+
+// Условие  SWITCH
+
+const num = 50;
+
+switch (num) {
+    case 49:
+        console.log('Неверно');
+        break;
+    case 50:
+        console.log('В точку!');
+        break;
+    default:
+        console.log('Не в этот раз!');
+        break;
+}
+
+
